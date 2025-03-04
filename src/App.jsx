0e89c1dacb71e91da279dbb90f884c07794ff64a
levionthemic <liem.levicoding834124@gmail.com>
2025-03-04 22:55:37 +0700

    Basic Register Form UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import LoginForm from '~/pages/Auth/LoginForm'
+import RegisterForm from '~/pages/Auth/RegisterForm'
 
 function App() {
   return (
     <Routes>
       <Route path='/login' element={<LoginForm />} />
+      <Route path='/register' element={<RegisterForm />} />
     </Routes>
   )
 }
